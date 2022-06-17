e6229a0901a7f7d3e7a4734fe9f45da7085009f1
EdwinVanJaarsveldt <u20474042@tuks.co.za>
2022-06-17 09:14:50 +0200

    Interface repository and repository ExpenseType

---
+++ Models/ExpenseType/ExpenseTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPIWalletAPI.Models.ExpenseType
+{
+    public class ExpenseTypeRepository
+    {
+    }
+}
---
+++ Models/ExpenseType/IExpenseTypeRepository.cs
@@ -0,0 +1,18 @@
+﻿using EPIWalletAPI.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPIWalletAPI.Models
+{
+    public interface IExpenseTypeRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+
+        Task<bool> SaveChangesAsync();
+        Task<ExpenseType[]> getAllExpenseTypesAsync();
+        Task<ExpenseType> getExpenseType(string ExpenseTypeName);
+    }
+}
