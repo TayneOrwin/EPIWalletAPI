50fbc23bd61e84627be5a374a176682a89de37d6
Tayne Orwin <u20430002@tuks.co.za>
2022-07-11 18:19:31 +0200

    Changes

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,67 @@
+﻿using EPIWalletAPI.Models;
+using EPIWalletAPI.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace EPIWalletAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        private readonly UserManager<AppUser> userManager;
+        private readonly IUserClaimsPrincipalFactory<AppUser> userClaimsPrincipalFactory;
+        private UserManager<AppUser> _userManager;
+        private IUserClaimsPrincipalFactory<AppUser> _claimsPrincipalFactory;
+
+        public LoginController(UserManager<AppUser> userManager
+            , IUserClaimsPrincipalFactory<AppUser> userClaimsPrincipalFactory)
+        {
+            _userManager = userManager;
+            _claimsPrincipalFactory = userClaimsPrincipalFactory;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [HttpPost]
+        [Route("Login")]
+         public async Task<IActionResult> Login(UserViewModel uvm)
+        {
+
+            var user = await _userManager.FindByNameAsync(uvm.EmailAddress);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, uvm.Password))
+            {
+                try
+                {
+                    var principal = await _claimsPrincipalFactory.CreateAsync(user);
+                    await HttpContext.SignInAsync(IdentityConstants.ApplicationScheme, principal);
+                }
+                catch(Exception)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, "Internal error");
+                }
+            }
+            return Ok();
+        }
+    }
+}
