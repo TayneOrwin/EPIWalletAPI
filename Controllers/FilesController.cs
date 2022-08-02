e91097342d01ac3649ee182fde41f8bfaa0a6d70
Tayne Orwin <u20430002@tuks.co.za>
2022-08-02 19:58:37 +0200

    receipt part 1

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,27 @@
+﻿using EPIWalletAPI.Models;
+using EPIWalletAPI.Models.Entities;
+using EPIWalletAPI.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace EPIWalletAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController: ControllerBase
+    {
+      
+
+
+
+
+
+
+
+    }
+}
---
+++ ViewModels/Wallpaper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPIWalletAPI.ViewModels
+{
+    public class Receipt
+    {
+        public int id { get; set; }
+        public byte[] upload { get; set; }
+    }
+}
