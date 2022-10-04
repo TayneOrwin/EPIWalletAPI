1df2028085922ea16621e75789ef1ddbf2cb3c50
Tayne Orwin <u20430002@tuks.co.za>
2022-10-04 04:06:25 +0200

    business rule

---
+++ Models/ExpenseValue/IExpenseValueRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EPIWalletAPI.Models
+{
+    public interface IAdminTimerRepository
+    {
+        Task<bool> SaveChangesAsync();
+        Task<Models.Entities.AdminTimer> getTimer(int timer);
+    }
+}
+
