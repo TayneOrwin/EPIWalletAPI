ea1b37170725da97801c6b6e96f57c83083ccfbd
EdwinVanJaarsveldt <u20474042@tuks.co.za>
2022-10-04 05:01:45 +0200

    reports

---
+++ Migrations/20221004021542_expensevalue.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPIWalletAPI.Migrations
+{
+    public partial class expensevalue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExpenseValue",
+                columns: table => new
+                {
+                    valueID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    value = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseValue", x => x.valueID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpenseValue");
+        }
+    }
+}
